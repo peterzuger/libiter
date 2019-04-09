325765874925d1bca5991e499a2101bd010b53cf
Peter Züger <zueger.peter@icloud.com>
2019-04-09 07:13:29 +0000

    added libiter::reverse test

---
+++ test/reverse_test.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file   libiter/test/reverse_test.cpp
+ * @author Peter Zueger
+ * @date   5.04.2019
+ * @brief  libiter::reverse tests
+ */
+#include <reverse.hpp>
+#include <iostream>
+#include <string>
+#include <array>
+#include <list>
+#include <deque>
+#include <sstream>
+
+int main(){
+    {
+        std::string txt{"abcdefghijklmnopqrstuvwxyz"};
+        std::string output{"zyxwvutsrqponmlkjihgfedcba"};
+
+        std::stringstream str;
+        for(const auto& i : libiter::reverse(txt))
+            str << i;
+        std::cout << str.str() << "\n" << output << "\n";
+        if(str.str() != output){
+            std::cerr << "failed\n";
+            return 1;
+        }
+    }
+    {
+        std::array<char,26> txt{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        std::string output{"zyxwvutsrqponmlkjihgfedcba"};
+
+        std::stringstream str;
+        for(const auto& i : libiter::reverse(txt))
+            str << i;
+        std::cout << str.str() << "\n" << output << "\n";
+        if(str.str() != output){
+            std::cerr << "failed\n";
+            return 1;
+        }
+    }
+    {
+        std::list<char> txt{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        std::string output{"zyxwvutsrqponmlkjihgfedcba"};
+
+        std::stringstream str;
+        for(const auto& i : libiter::reverse(txt))
+            str << i;
+        std::cout << str.str() << "\n" << output << "\n";
+        if(str.str() != output){
+            std::cerr << "failed\n";
+            return 1;
+        }
+    }
+    {
+        std::deque<char> txt{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        std::string output{"zyxwvutsrqponmlkjihgfedcba"};
+
+        std::stringstream str;
+        for(const auto& i : libiter::reverse(txt))
+            str << i;
+        std::cout << str.str() << "\n" << output << "\n";
+        if(str.str() != output){
+            std::cerr << "failed\n";
+            return 1;
+        }
+    }
+}
