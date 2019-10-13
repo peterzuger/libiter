4f97c59ba93c2944c04fce7ed65706dd7bc767fc
Peter Züger <zueger.peter@icloud.com>
2019-10-13 11:28:15 +0200

    added license to header comment

--- include/enumerate_iterator.hpp
+++ include/enumerate_iterator.hpp
@@ -3,6 +3,21 @@
  * @author Peter Züger
  * @date   17.04.2019
  * @brief  enumerate iterators
+ *
+ * This file is part of libiter (https://gitlab.com/peterzuger/libcrypt).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef LIBITER_ENUMERATE_ITERATOR_HPP
 #define LIBITER_ENUMERATE_ITERATOR_HPP
--- test/reverse_test.cpp
+++ test/reverse_test.cpp
@@ -3,6 +3,21 @@
  * @author Peter Züger
  * @date   05.04.2019
  * @brief  libiter::reverse tests
+ *
+ * This file is part of libiter (https://gitlab.com/peterzuger/libcrypt).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <reverse.hpp>
 #include <iostream>
